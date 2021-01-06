a68b1e0f40735c0951593e2137247c210609a20e
Chi-Hao <73461539+Chi-Hao-Yu@users.noreply.github.com>
2021-01-06 11:09:48 +0800

    Add files via upload
    非靜態類別，需實作其類別才能使用其方法

---
+++ MyClass.java
@@ -0,0 +1,24 @@
+package method;
+
+public class MyClass {
+void sub(int x,int y) {
+	System.out.print("呼叫MyClass的非靜態sub方法-->");
+	System.out.println(x+"-"+y+"="+(x-y));
+}
+
+public static void main(String[]args) {
+	int a=25;
+	MyClass c1=new MyClass();
+	c1.sub(a-5, 3);
+	MyCal c2=new MyCal();
+	c2.sub(a+3,5);
+}
+}
+
+class MyCal{
+	void sub(int x,int y) {
+		System.out.print("呼叫MyCal類別的非靜態sub方法-->");
+		System.out.println(x+"-"+y+"="+(x-y));
+	}
+}
+
